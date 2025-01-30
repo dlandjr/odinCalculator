06a845b8d08abf611dd1cb6cb78393fd044c4d22
dlandjr <donald.landjr@gmail.com>
2025-01-30 14:03:01 -0800

    Update javascript operator functions to use Number instead of parseFloat

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function add(a, b) {
-    return Number((parseFloat(a) + parseFloat(b)).toFixed(5));
+    return Number((Number(a) + Number(b)).toFixed(5));
 }
 function subtract(a, b) {
-    return Number((parseFloat(a) - parseFloat(b)).toFixed(5));
+    return Number((Number(a) - Number(b)).toFixed(5));
 }
 function multiply(a, b) {
-    return Number((parseFloat(a) * parseFloat(b)).toFixed(5));
+    return Number((Number(a) * Number(b)).toFixed(5));
 }
 function divide(a, b) {
     return b === "0" || b === 0
       ? "NOPE"
-      : Number((parseFloat(a) / parseFloat(b)).toFixed(5));
+      : Number((Number(a) / Number(b)).toFixed(5));
 }
   
 const operators = {
@@ -38,34 +38,3 @@ const elements = {
     operatorButtons: document.querySelectorAll(".operator"),
     backspaceButton: document.querySelector(".backspace"),
 };
-  
-  console.log(elements.operatorButtons);
-  
-  const updateDisplay = (content) => {
-    elements.display.textContent = content;
-  };
-  
-  const updateCalculationDisplay = (content) => {
-    elements.calculationDisplay.textContent = content;
-  };
-
-const keyboardMap = {
-    Enter: () => elements.equalsButton.click(),
-    Backspace: () => elements.backspaceButton.click(),
-    Escape: () => elements.clearAllButton.click(),
-    "+": () => elements.operatorButtons[3].click(),
-    "-": () => elements.operatorButtons[2].click(),
-    "*": () => elements.operatorButtons[1].click(),
-    "/": () => elements.operatorButtons[0].click(),
-    ".": () => elements.commaButton.click(),
-};
-
-for (let i = 0; i <= 9; i++) {
-  keyboardMap[i.toString()] = () => {
-    const numberButton = Array.from(elements.numberButtons).find(
-      (button) => button.textContent === i.toString()
-    );
-    if (numberButton) numberButton.click();
-  };
-}
-
